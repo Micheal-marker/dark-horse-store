09db15a4c81b28da6ab6dc3a030eb90b7f34b909
Micheal-marker <1557573258@qq.com>
2025-10-15 19:05:39 +0800

    分布式事务-XA模式

--- hmall/pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
+++ hmall/pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
@@ -14,6 +14,7 @@ import com.hmall.pay.domain.po.PayOrder;
 import com.hmall.pay.enums.PayStatus;
 import com.hmall.pay.mapper.PayOrderMapper;
 import com.hmall.pay.service.IPayOrderService;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
